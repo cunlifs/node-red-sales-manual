5976a49e5940ba970ab241499a6bedaed8aece1d
Stu Cunliffe <s_cunliffe@uk.ibm.com>
2019-11-27 11:47:18 +0000

    Update sales_manual_finder.py

--- sales_manual_finder.py
+++ sales_manual_finder.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
-​
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import json
 import sys
-​
+
 MTM = sys.argv[1]
 # MTM = "9080-MME"
 Search_url = "https://www-01.ibm.com/common/ssi/FetchJSON.wss?ctype=DD%3ADDSM&ctry=EUR%3AGB&lang=&MPPEFFTR=DOCNO&MPPEFSCH="
 Search_url += MTM
 Search_url += "&MPPEFFDR=&MPPEFTDR=&MPPEFSRT=2&hitsperpage=20&resultpage=1"
-​
+
 html = urlopen(Search_url)
 soup = BeautifulSoup(html, 'html')
 # soup.content
