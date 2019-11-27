235871bb81bd268bc30502404e9a7fd8a7698632
Stu Cunliffe <s_cunliffe@uk.ibm.com>
2019-11-27 13:59:52 +0000

    Update copy_flow.sh

--- copy_flow.sh
+++ copy_flow.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Copy flows file to new location
 chmod 750 /usr/src/node-red/sales-manual-reader-flow.json
-HOSTNAME_SHORT='hostname -s'
+HOSTNAME_SHORT=`hostname -s`
 mv /usr/src/node-red/sales-manual-reader-flow.json /usr/src/node-red/.node-red/flows_$HOSTNAME_SHORT.json
